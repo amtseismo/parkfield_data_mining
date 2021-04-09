86f5e9fc71b226b4e4bc84b0740d937d38ec545d
bgd88 <delbridge.brent@gmail.com>
2021-04-09 13:55:25 -0600

    Fingers Crossed

--- scripts/ncedc_query_wMultithreading.py
+++ scripts/ncedc_query_wMultithreading.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 10 15:19:41 2019
 
-Script to make query files for the ncedc
+Script to make query files for the ncedc in parallel
 
-@author: amt
+@author: amt, BGD
 """
 import os
 from datetime import datetime, timedelta
@@ -13,7 +12,7 @@ from obspy.core import UTCDateTime
 import subprocess
 from obspy.clients.fdsn import Client
 import multiprocessing
-
+from multiprocessing.pool import ThreadPool
 # smoke 'em if you got 'em
 n_processor = multiprocessing.cpu_count()
 
@@ -55,19 +54,20 @@ for net in inventory:
 
 # Request
 date_list = []
-date = start_time._get_date()
-while date + timedelta(days=1) < end_time._get_date():
-    print(date)
+date = start_time._get_datetime()
+while date + timedelta(days=1) < end_time._get_datetime():
+    print(date.isoformat())
     next_date = date + timedelta(days=1)
     date_list.append(date)
     date = next_date
 
 def get_day(date):
     next_date = date + timedelta(days=1)
+    date_str = date.isoformat().split('T')[0]
     doy = date.timetuple().tm_yday
-    for sta_str in sta_comp_list.keys():
-        for comp_str in sta_comp_list[sta_str]:
-            file_name = f"{str(date)}.{net_str}.{sta_str}.{comp_str}.ms"
+    for sta_str, comp_list in sta_comp_list.items():
+        for comp_str in comp_list:
+            file_name = f"{date_str}.{net_str}.{sta_str}.{comp_str}.ms"
             file_path = os.path.join(f"../data/{sta_str}", file_name)
             print(file_path)
             ## BK CMB -- BHZ 2010-03-25T00:00:00 2010-04-01T00:00:00
@@ -80,5 +80,5 @@ def get_day(date):
             command=f"curl --data-binary @waveform.request -o {file_path} http://service.ncedc.org/fdsnws/dataselect/1/query"
             subprocess.call(command, shell=True)
 
-with multiprocessing.pool.ThreadPool(n_processor) as p:
+with ThreadPool(n_processor) as p:
     p.map(get_day, date_list)
