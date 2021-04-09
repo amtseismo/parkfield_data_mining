5ede82bacaac692f4fd21a491b6085c5812d7983
bgd88 <delbridge.brent@gmail.com>
2021-04-08 19:23:38 -0600

    Fixed directory structure to match apply_cnn

--- ncedc_query_wMultithreading.py
+++ ncedc_query_wMultithreading.py
@@ -7,7 +7,7 @@ Script to make query files for the ncedc
 
 @author: amt
 """
-
+import os
 from datetime import datetime, timedelta
 from obspy.core import UTCDateTime
 import subprocess
@@ -17,10 +17,10 @@ import multiprocessing
 # smoke 'em if you got 'em
 n_processor = multiprocessing.cpu_count()
 
-data_dir = "../data"
+out_dir = "../data"
 client_list = ['NCEDC']
 start_time = UTCDateTime("2018-01-01")
-end_time = UTCDateTime("2019-01-03")
+end_time = UTCDateTime("2019-01-01")
 center_lon = -120.374
 center_lat = 35.815
 sta_radius = 0.5
@@ -28,10 +28,10 @@ sta_radius = 0.5
 
 station_list = {}
 cl = client_list[0]
-net = "BP"  # "*"
+net_str = "BP"  # "*"
 sta_wcard = "*"  #GHIB"
 chan_wcard = "SH*,BH*,EH*,HH*,DP*,LH*"
-inventory = Client(cl).get_stations(network=net, station=sta_wcard,
+inventory = Client(cl).get_stations(network=net_str, station=sta_wcard,
                                     channel=chan_wcard,
                                     latitude=center_lat, longitude=center_lon,
                                     maxradius=sta_radius, level='channel',
@@ -41,18 +41,23 @@ inventory = Client(cl).get_stations(network=net, station=sta_wcard,
 # Get list to download from invetory
 sta_comp_list = {}
 for net in inventory:
-    net_str = net.code
+    net_code = net.code
     for sta in net:
-        sta_str = sta.code
+        sta_out_dir = os.path.join(out_dir, f'{sta.code}')
+        if not os.path.exists(sta_out_dir):
+            os.makedirs(sta_out_dir)
+        comp_list = []
         for comp in sta:
             comp_list.append(comp.code)
-        sta_comp_list[sta_str] = list(set(comp_list))
+        if len(comp_list)==3:
+            sta_comp_list[sta.code] = list(set(comp_list))
 
 
 # Request
 date_list = []
 date = start_time._get_date()
-while date + timedelta(days=1) < end_time:
+while date + timedelta(days=1) < end_time._get_date():
+    print(date)
     next_date = date + timedelta(days=1)
     date_list.append(date)
     date = next_date
@@ -60,20 +65,20 @@ while date + timedelta(days=1) < end_time:
 def get_day(date):
     next_date = date + timedelta(days=1)
     doy = date.timetuple().tm_yday
-    for sta in sta_comp_list.keys():
-        for comp in sta_comp_list[sta_str]:
-            file = str(date.year) + '_' + str(doy).zfill(3) + '_' + sta
-            print(file)
+    for sta_str in sta_comp_list.keys():
+        for comp_str in sta_comp_list[sta_str]:
+            file_name = f"{str(date)}.{net_str}.{sta_str}.{comp_str}.ms"
+            file_path = os.path.join(f"../data/{sta_str}", file_name)
+            print(file_path)
             ## BK CMB -- BHZ 2010-03-25T00:00:00 2010-04-01T00:00:00
-            tmp = f'NC {sta} * {comp} {date.isoformat()} {next_date.isoformat()}\n'
+            tmp = f'{net_str} {sta_str} * {comp_str} {date.isoformat()} {next_date.isoformat()}\n'
             print(tmp)
-            # f = open('waveform.request', "w")
-            # f.write(tmp)
-            # f.close()
-            # # curl --data-binary @waveform.request -o BK.miniseed http://service.ncedc.org/fdsnws/dataselect/1/query
-            # command=f"curl --data-binary @waveform.request -o {file}.miniseed http://service.ncedc.org/fdsnws/dataselect/1/query"
-            # subprocess.call(command, shell=True)
+            f = open('waveform.request', "w")
+            f.write(tmp)
+            f.close()
+            # curl --data-binary @waveform.request -o BK.miniseed http://service.ncedc.org/fdsnws/dataselect/1/query
+            command=f"curl --data-binary @waveform.request -o {file_path} http://service.ncedc.org/fdsnws/dataselect/1/query"
+            subprocess.call(command, shell=True)
 
-get_day(date_list[0])
-# with multiprocessing.pool.ThreadPool(n_processor) as p:
-#     p.map(get_day, date_list)
+with multiprocessing.pool.ThreadPool(n_processor) as p:
+    p.map(get_day, date_list)
