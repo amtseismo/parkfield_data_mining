beba171609217324f5304cee3752c01a97ccab14
delbridge <delbridge@pn2032696.lanl.gov>
2021-04-10 09:17:43 -0600

    Added Logging

--- scripts/ncedc_query_wMultithreading.py
+++ scripts/ncedc_query_wMultithreading.py
@@ -14,23 +14,34 @@ import subprocess
 from obspy.clients.fdsn import Client
 import multiprocessing
 from multiprocessing.pool import ThreadPool
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+now_str = datetime.now().strftime("%b-%d-%Y_%H-%M-%S")
+fh = logging.FileHandler(f'data_pull_{now_str}.log')
+fh.setLevel(logging.INFO)
+logger.addHandler(fh)
+
 # smoke 'em if you got 'em
 n_processor = multiprocessing.cpu_count()
 
 out_dir = "../data"
 client_list = ['NCEDC']
-start_time = UTCDateTime("2018-01-01")
+start_time = UTCDateTime("2018-12-21")
 end_time = UTCDateTime("2019-01-01")
 center_lon = -120.374
 center_lat = 35.815
-sta_radius = 0.5
+sta_radius = 1
 
 
 station_list = {}
 cl = client_list[0]
-net_str = "BP"  # "*"
+net_str = "BP,BK,NC"  # "*"
 sta_wcard = "*"  #GHIB"
-chan_wcard = "SH*,BH*,EH*,HH*,DP*,LH*"
+chan_wcard = "BH*,HH*,DP*,LH*"
 inventory = Client(cl).get_stations(network=net_str, station=sta_wcard,
                                     channel=chan_wcard,
                                     latitude=center_lat, longitude=center_lon,
@@ -39,9 +50,9 @@ inventory = Client(cl).get_stations(network=net_str, station=sta_wcard,
                                     starttime=start_time, endtime=end_time)
 
 # Get list to download from invetory
-sta_comp_list = {}
+net_sta_comp_list = {}
 for net in inventory:
-    net_code = net.code
+    sta_comp_list = {}
     for sta in net:
         sta_out_dir = os.path.join(out_dir, f'{sta.code}')
         if not os.path.exists(sta_out_dir):
@@ -49,9 +60,12 @@ for net in inventory:
         comp_list = []
         for comp in sta:
             comp_list.append(comp.code)
-        if len(comp_list)==3:
+        if len(comp_list) == 3:
             sta_comp_list[sta.code] = list(set(comp_list))
+    net_sta_comp_list[net.code] = sta_comp_list
 
+with open('net_sta_comp_list.json', 'w') as fp:
+    json.dump(net_sta_comp_list, fp)
 
 # Request
 date_list = []
@@ -62,26 +76,34 @@ while date + timedelta(days=1) < end_time._get_datetime():
     date_list.append(date)
     date = next_date
 
+
 def get_day(date):
     next_date = date + timedelta(days=1)
     date_str = date.isoformat().split('T')[0]
     doy = date.timetuple().tm_yday
-    for sta_str, comp_list in sta_comp_list.items():
-        for comp_str in comp_list:
-            file_name = f"{date_str}.{net_str}.{sta_str}.{comp_str}.ms"
-            file_path = os.path.join(f"../data/{sta_str}", file_name)
-            print(file_path)
-            ## BK CMB -- BHZ 2010-03-25T00:00:00 2010-04-01T00:00:00
-            tmp = f'{net_str} {sta_str} * {comp_str} {date.isoformat()} {next_date.isoformat()}\n'
-            print(tmp)
-            scriptFile = tempfile.NamedTemporaryFile(delete=True)
-            with open(scriptFile.name, 'w') as f:
-                f.write(tmp)
-                f.close()
-                # curl --data-binary @waveform.request -o BK.miniseed http://service.ncedc.org/fdsnws/dataselect/1/query
-                command=f"curl --data-binary @{f.name} -o {file_path} http://service.ncedc.org/fdsnws/dataselect/1/query"
-                print(command)
-                subprocess.call(command, shell=True)
+    for net_str, sta_comp_list in net_sta_comp_list.items():
+        for sta_str, comp_list in sta_comp_list.items():
+            for comp_str in comp_list:
+                # Filename and path of datafile to write
+                file_name = f"{date_str}.{net_str}.{sta_str}.{comp_str}.ms"
+                file_path = os.path.join(f"../data/{sta_str}", file_name)
+                logger.info(f"\n\n ** Trying: {file_name} **")
+
+                # Query request
+                tmp = f'{net_str} {sta_str} * {comp_str} {date.isoformat()} {next_date.isoformat()}\n'
+                logger.info(f"Query cmd: {tmp}")
+                scriptFile = tempfile.NamedTemporaryFile(delete=True)
+                http_str = "http://service.ncedc.org/fdsnws/dataselect/1/query"
+                with open(scriptFile.name, 'w') as f:
+                    f.write(tmp)
+                    f.close()
+                    # curl --data-binary @waveform.request -o
+                    # BK.miniseed http://service.ncedc.org/fdsnws/dataselect/1/query
+                    cmd = f"curl --data-binary @{f.name} -o {file_path} {http_str}"
+                    logger.info(f"curl call: {cmd}")
+                    subprocess.call(cmd, shell=True)
+
+
 
 with ThreadPool(n_processor) as p:
     p.map(get_day, date_list)
